8e1936497eafdc460d1b429fa6866d0e6c5652ab
gowtham4545 <gowthamgopi444@gmail.com>
2023-05-07 16:18:20 +0530

    basic working

---
+++ src/Components/Form/index.js
@@ -0,0 +1,203 @@
+import { redirect, useNavigate } from 'react-router-dom';
+import './index.css';
+import React, { useEffect, useState } from 'react'
+
+const Index = () => {
+    const [page, setPage] = useState(1);
+    const [p1t1, setp1t1] = useState();
+    const [p1t2, setp1t2] = useState();
+    const [p1t3, setp1t3] = useState();
+    const [p1t4, setp1t4] = useState();
+    const [p1t5, setp1t5] = useState();
+    const [p1r1, setp1r1] = useState();
+    const [p1r2, setp1r2] = useState();
+    const [p2t1, setp2t1] = useState();
+    const [p2t2, setp2t2] = useState();
+    const [p2t3, setp2t3] = useState();
+    const [p2t4, setp2t4] = useState();
+    const [p2t5, setp2t5] = useState();
+    const [p2r1, setp2r1] = useState();
+    const [p2r2, setp2r2] = useState();
+    const [p3slt, setp3slt] = useState();
+    const [p3ch, setp3ch] = useState();
+    const [progress, setProgress] = useState(0);
+
+    const navigate=useNavigate();
+
+    function nextPage() {
+        if(progress===100){
+            redirect(`review`)
+            navigate(`./review?p1t1=${p1t1}&p1t2=${p1t2}&p1t3=${p1t3}&p1t4=${p1t4}&p1t5=${p1t5}&p1r1=${p1r1}&p1r2=${p1r2}&p2t1=${p2t2}&p2t3=${p2t3}&p2t4=${p2t4}&p2t5=${p2t5}&p2r1=${p2r1}&p2r2=${p2r2}&p3slt=${p3slt}&p3ch=${p3ch}`)
+        }
+        else if (progress === (7 / 16 * 100))
+            setPage(page + 1);
+        else if (progress === (14 / 16 * 100))
+            setPage(page + 1);
+        else if (page === 3 && p3ch !== 'Yes')
+            alert('Please agree to the conditions');
+        else alert('Fill all the fields!');
+        console.log(page);
+        console.log(progress);
+    }
+
+    function Progress() {
+        let count = 0;
+        count += (p1t1 ? 1 : 0);
+        count += (p1t2 ? 1 : 0);
+        count += (p1t3 ? 1 : 0);
+        count += (p1t4 ? 1 : 0);
+        count += (p1t5 ? 1 : 0);
+        count += (p2t1 ? 1 : 0);
+        count += (p2t2 ? 1 : 0);
+        count += (p2t3 ? 1 : 0);
+        count += (p2t4 ? 1 : 0);
+        count += (p2t5 ? 1 : 0);
+        count += (p1r1 ? 1 : 0);
+        count += (p1r2 ? 1 : 0);
+        count += (p2r1 ? 1 : 0);
+        count += (p2r2 ? 1 : 0);
+        count += (p3slt ? 1 : 0);
+        count += (p3ch ? 1 : 0);
+        setProgress(count / 16 * 100);
+    }
+
+    let item1 = ['Name', "Father Name", 'Mother Name', 'City', 'State', ['Male', 'Female'], ['Rural', 'Urban']]
+    let item2 = ['College Name', 'City', 'State', 'Percentage', 'Grade', ['ICSE', 'CBSE'], ['PCMB', 'PCMC']]
+
+    function renderSwitch(page) {
+        switch (page) {
+            case 1:
+                return <>
+                    <div className='page'>
+                        page 1
+                        <div action={null}>
+                            <div className='input'>
+                                <div className='label'>{item1[0]}</div>
+                                <input className='t' type='' onChange={(e) => setp1t1(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item1[1]}</div>
+                                <input className='t' type='' onChange={(e) => setp1t2(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item1[2]}</div>
+                                <input className='t' type='' onChange={(e) => setp1t3(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item1[3]}</div>
+                                <input className='t' type='' onChange={(e) => setp1t4(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item1[4]}</div>
+                                <input className='t' type='' onChange={(e) => setp1t5(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <input className='r' type='radio' onChange={(e) => setp1r1(e.target.value)} name='p1r1' value={item1[5][0]} required />
+                                <div className='label'>{item1[5][0]}</div>
+                                <input className='r' type='radio' onChange={(e) => setp1r1(e.target.value)} name='p1r1' value={item1[5][1]} required />
+                                <div className='label'>{item1[5][1]}</div>
+                            </div>
+                            <div className='input'>
+                                <input className='r' type='radio' onChange={(e) => setp1r2(e.target.value)} name='p1r2' value={item1[6][0]} required />
+                                <div className='label'>{item1[6][0]}</div>
+                                <input className='r' type='radio' onChange={(e) => setp1r2(e.target.value)} name='p1r2' value={item1[6][1]} required />
+                                <div className='label'>{item1[6][1]}</div>
+                            </div>
+                            <div className='input'>
+                                <div className='label'>Photo</div>
+                                <input className='f' type='file' required />
+                            </div>
+                            <input type='button' onClick={nextPage} value={'Next'} />
+                        </div>
+                    </div>
+                </>
+
+            case 2:
+                return <>
+                    <div className='page'>
+                        page 2
+                        <form>
+                            <div className='input'>
+                                <div className='label'>{item2[0]}</div>
+                                <input className='t' type='' onChange={(e) => setp2t1(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item2[1]}</div>
+                                <input className='t' type='' onChange={(e) => setp2t2(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item2[2]}</div>
+                                <input className='t' type='' onChange={(e) => setp2t3(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item2[3]}</div>
+                                <input className='t' type='' onChange={(e) => setp2t4(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <div className='label'>{item2[4]}</div>
+                                <input className='t' type='' onChange={(e) => setp2t5(e.target.value)} required />
+                            </div>
+                            <div className='input'>
+                                <input className='r' type='radio' onChange={(e) => setp2r1(e.target.value)} name='p1r1' value={item2[5][0]} required />
+                                <div className='label'>{item2[5][0]}</div>
+                                <input className='r' type='radio' onChange={(e) => setp2r1(e.target.value)} name='p1r1' value={item2[5][1]} required />
+                                <div className='label'>{item2[5][1]}</div>
+                            </div>
+                            <div className='input'>
+                                <input className='r' type='radio' onChange={(e) => setp2r2(e.target.value)} name='p1r2' value={item2[6][0]} required />
+                                <div className='label'>{item2[6][0]}</div>
+                                <input className='r' type='radio' onChange={(e) => setp2r2(e.target.value)} name='p1r2' value={item2[6][1]} required />
+                                <div className='label'>{item2[6][1]}</div>
+                            </div>
+                            <div className='input'>
+                                <div className='label'>Marks Card</div>
+                                <input className='f' type='file' required />
+                            </div>
+                            <input type='submit' onClick={nextPage} value={'Next'} />
+                        </form>
+                    </div>
+                </>
+
+            case 3:
+                return <>
+                    <div className='page'>
+                        page 3 
+                        <div className='input'>
+                            <div className='label'>Course</div>
+                            <select className='f' type='' onLoad={(e) => setp3slt(e.target.value)} onClick={(e) => setp3slt(e.target.value)} required >
+                                <option value='B.Tech'  onClick={(e) => setp3slt(e.target.value)}>Select One</option>
+                                <option value='B.Tech' defaultChecked onClick={(e) => setp3slt(e.target.value)}>B.Tech</option>
+                                <option value='B.Arch' onClick={(e) => setp3slt(e.target.value)}>B.Arch</option>
+                                <option value='B.Sc' onClick={(e) => setp3slt(e.target.value)}>B.Sc</option>
+                                <option value='B.Com' onClick={(e) => setp3slt(e.target.value)}>B.Com</option>
+                            </select>
+                        </div>
+                        <div className='input'>
+                            <div className='label'></div>
+                            <input className='f' type='checkbox' onChange={(e) => setp3ch(e.target.value)} value={'Yes'} required />
+                        </div>
+                    </div>
+                    <input className='submit' type='submit' onClick={nextPage} value={'Submit'}/>
+                </>
+
+            default:
+                return null;
+        }
+    }
+
+    useEffect(Progress);
+
+    return (
+        <div>
+            <h1>Admission Form</h1>
+            {
+                renderSwitch(page)
+            }
+            <div className='progress' style={{ width: `${progress}vw` }}></div>
+        </div>
+    )
+}
+
+
+
+export default Index;
---
+++ src/Components/View/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import './index.css';
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+    const { search } = useLocation();
+
+    return search;
+}
+
+function Index() {
+    const search = useQuery();
+    const params = React.useMemo(() => new URLSearchParams(search), [search]);
+    const p1t1 = params.get('p1t1')
+    const p1t2 = params.get('p1t2')
+    const p1t3 = params.get('p1t3')
+    const p1t4 = params.get('p1t4')
+    const p1t5 = params.get('p1t5')
+    const p2t1 = params.get('p2t1')
+    const p2t2 = params.get('p2t2')
+    const p2t3 = params.get('p2t3')
+    const p2t4 = params.get('p2t4')
+    const p2t5 = params.get('p2t5')
+    const p2r1 = params.get('p2r1')
+    const p2r2 = params.get('p2r2')
+    const p1r1 = params.get('p1r1')
+    const p1r2 = params.get('p1r2')
+    const p3ch = params.get('p3ch')
+    const p3slt = params.get('p3slt')
+
+    const labels = ['Name', "Father Name", 'Mother Name', 'City', 'State', 'Gender', 'Area', 'College Name', 'City', 'State', 'Percentage', 'Grade', 'Syllabus', 'Subjects', 'Course', 'Agree with conditions']
+    const arr = [p1t1, p1t2, p1t3, p1t4, p1t5, p1r1, p1r2, p2t1, p2t2, p2t3, p2t4, p2t5, p2r1, p2r2, p3slt, p3ch]
+    console.log(arr.length);
+    return (
+        <div>
+            <h1>Review</h1>
+            <div className='details'>
+                Personal Details
+                {arr[0] ? (<div className='detail'>
+                    <div className='label'>{labels[0]}</div>
+                    <div className='content'>{`${arr[0]}`}</div>
+                </div>) : <></>}
+                {arr[1] ? (<div className='detail'>
+                    <div className='label'>{labels[1]}</div>
+                    <div className='content'>{`${arr[1]}`}</div>
+                </div>) : <></>}
+                {arr[2] ? (<div className='detail'>
+                    <div className='label'>{labels[2]}</div>
+                    <div className='content'>{`${arr[2]}`}</div>
+                </div>) : <></>}
+                {arr[3] ? (<div className='detail'>
+                    <div className='label'>{labels[3]}</div>
+                    <div className='content'>{`${arr[3]}`}</div>
+                </div>) : <></>}
+                {arr[4] ? (<div className='detail'>
+                    <div className='label'>{labels[4]}</div>
+                    <div className='content'>{`${arr[4]}`}</div>
+                </div>) : <></>}
+                {arr[5] ? (<div className='detail'>
+                    <div className='label'>{labels[5]}</div>
+                    <div className='content'>{`${arr[5]}`}</div>
+                </div>) : <></>}
+                {arr[6] ? (<div className='detail'>
+                    <div className='label'>{labels[6]}</div>
+                    <div className='content'>{`${arr[6]}`}</div>
+                </div>) : <></>}
+                Study Details
+                {arr[7] ? (<div className='detail'>
+                    <div className='label'>{labels[7]}</div>
+                    <div className='content'>{`${arr[7]}`}</div>
+                </div>) : <></>}
+                {arr[8] ? (<div className='detail'>
+                    <div className='label'>{labels[8]}</div>
+                    <div className='content'>{`${arr[8]}`}</div>
+                </div>) : <></>}
+                {arr[9] ? (<div className='detail'>
+                    <div className='label'>{labels[9]}</div>
+                    <div className='content'>{`${arr[9]}`}</div>
+                </div>) : <></>}
+                {arr[10] ? (<div className='detail'>
+                    <div className='label'>{labels[10]}</div>
+                    <div className='content'>{`${arr[10]}`}</div>
+                </div>) : <></>}
+                {arr[11] ? (<div className='detail'>
+                    <div className='label'>{labels[11]}</div>
+                    <div className='content'>{`${arr[11]}`}</div>
+                </div>) : <></>}
+                {arr[12] ? (<div className='detail'>
+                    <div className='label'>{labels[12]}</div>
+                    <div className='content'>{`${arr[12]}`}</div>
+                </div>) : <></>}
+                {arr[13] ? (<div className='detail'>
+                    <div className='label'>{labels[13]}</div>
+                    <div className='content'>{`${arr[13]}`}</div>
+                </div>) : <></>}
+                Admission
+                {arr[14] ? (<div className='detail'>
+                    <div className='label'>{labels[14]}</div>
+                    <div className='content'>{`${arr[14]}`}</div>
+                </div>) : <></>}
+                {arr[15] ? (<div className='detail'>
+                    <div className='label'>{labels[15]}</div>
+                    <div className='content'>{`${arr[15]}`}</div>
+                </div>) : <></>}
+                
+            </div>
+        </div>
+    )
+}
+
+export default Index;
